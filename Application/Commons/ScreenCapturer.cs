b7a2cbc0b7fabce00b11b20b2b95d3112dd12406
Sergio Zambrano <sergiouce16@gmail.com>
2025-07-16 17:20:35 -0500

    Avance

--- Application/Commons/ScreenCapturer.cs
+++ Application/Commons/ScreenCapturer.cs
@@ -10,23 +10,24 @@ namespace Application.Commons
         public Bitmap? CaptureWindow(IntPtr handle)
         {
             RECT rect;
+
+            // Wait 1 second before to capture screen
+            Thread.Sleep(1000);
             if (!WindowsAPI.GetWindowRect(handle, out rect))
             {
                 return null;
             }
 
+            // Compute screenshot
             var width = rect.Right - rect.Left;
             var height = rect.Bottom - rect.Top;
             var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-
-
             using (var gfxBmp = Graphics.FromImage(bmp))
             {
                 gfxBmp.CopyFromScreen(rect.Left, rect.Top, 0, 0, new Size(width, height), CopyPixelOperation.SourceCopy);
             }
 
             return bmp;
-
         }
     }
 }
